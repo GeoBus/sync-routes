95ae4826108acbae939436c1315620a501de4181
João de Vasconcelos <johnyvasconcelos@icloud.com>
2020-05-04 02:58:41 +0100

    Removed unecessary packages

--- index.js
+++ index.js
@@ -7,8 +7,8 @@
 
 /* * */
 /* IMPORTS */
-const config = require("config");
 const fs = require("fs");
+const config = require("config");
 const carrisAPI = require("./services/carrisAPI");
 
 const syncRoutes = async () => {
